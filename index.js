425297c7a99193d6ea181275c1bfdd70c2d7f37c
Dhwani Sarokar <dhwanisarokar@gmail.com>
2022-05-21 13:26:18 +0530

    create mildware static file that is assests and link css and js file link to home.ejs

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use('/', require('./routes'));
 
 //set up the view engine
 app.set('view engine', 'ejs');
-app.set('view', './views');
+app.set('views', './views');
+
+app.use(express.static('assets'));
 
 app.listen(port, function(err){
     if (err) {
