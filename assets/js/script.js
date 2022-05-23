d3b5f9b8d3ca4cf187148a325bafefa03fcb09c5
Dhwani Sarokar <dhwanisarokar@gmail.com>
2022-05-24 00:35:08 +0530

    Adding style and other setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
 console.log('Script is loaded.');
+
+const d = new Date();
+let text = d.toLocaleDateString('en-us', {weekday:"short", year:"numeric", month:"short", day:"numeric"})
+document.getElementById("date").innerHTML = text;
