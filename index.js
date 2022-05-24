ca3a0a20fdde6022764e6db87e5c171014c5db70
Dhwani Sarokar <dhwanisarokar@gmail.com>
2022-05-24 23:40:43 +0530

    populating the dataBAse

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const port = 8000;
 
+//require database
 const db = require('./config/mongoose');
+
+//require schema
+const Task = require('./models/TODO_List');
 
 const app = express();
 
@@ -20,7 +24,21 @@ app.use(express.static('assets'));
 
 //Getting data from user to server
 app.post('/create-task', function(req, res) {
-    console.log(req.body);
+    // console.log(req.body);
+
+    Task.create({
+        task: req.body.task,
+        category: req.body.category,
+        dueDate: req.body.dueDate
+    }, function (err, nweTask) {
+        if (err) {
+            console.log("Error in creating Task!");
+            return;
+        }
+
+        console.log('*****', nweTask);
+        return res.redirect('back');
+    });
 })
 
 app.listen(port, function(err){
